000410d43c2147b8e24c70d708b1a677bf46bc83
guru <gururaj.konded@gmail.com>
2025-03-24 17:36:32 +0530

    initial commit spring boot demo

---
+++ src/main/java/com/example/employeedeptapp/EmployeeDepartmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeedeptapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDepartmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDepartmentAppApplication.class, args);
+	}
+
+}
